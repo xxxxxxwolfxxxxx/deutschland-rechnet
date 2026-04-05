dd095a6d4e565ecf73055006e37b63201c276c9d
Matthias Dührkop <matthiasduhrkop@Matthiass-MacBook-Air.fritz.box>
2026-04-05 15:14:38 +0200

    fix: correct GitHub username in site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://matthiasduhrkop.github.io',
+  site: 'https://xxxxxxwolfxxxxx.github.io',
   base: '/deutschland-rechnet',
   trailingSlash: 'always',
   integrations: [sitemap()],
